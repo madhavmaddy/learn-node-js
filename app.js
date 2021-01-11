8e305a5deca71aa7d08294df0d8c82e7ef28d7ea
madhavmaddy <madhav@getchaine.com>
2021-01-11 19:11:25 +0530

    added first and last name

---
+++ app.js
@@ -0,0 +1,5 @@
+//import log and endpoint from logger.js
+const logger = require('./logger');
+
+logger.result('Hello')
+console.log(logger.endPoint)
---
+++ logger.js
@@ -0,0 +1,12 @@
+
+console.log(__filename);
+console.log(__dirname);
+const url = "http://logger.io"
+//exporting this function to app.js to print the result
+function log(message) {
+    console.log(message)
+}
+
+module.exports.result = log;
+
+module.exports.endPoint = url;
