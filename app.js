1a0b83ebca017bd23bf2e18fb9dd52a6b00ed886
madhavmaddy <madhav@getchaine.com>
2021-01-11 21:11:27 +0530

    raise an event

--- app.js
+++ app.js
@@ -1,5 +1,39 @@
 //import log and endpoint from logger.js
-const logger = require('./logger');
+// const logger = require('./logger');
 
-logger.result('Hello')
-console.log(logger.endPoint)
+
+// logger.result('Hello')
+// console.log(logger.endPoint)
+
+//path module in node js
+// const path = require('path');
+// const pathObj = path.parse(__filename);
+// console.log(pathObj.base)
+
+//Os module in node js
+// const os = require('os'); 
+// const freeMemory = os.freemem();
+// const upTime = os.uptime();
+// const totalMemory = os.totalmem();
+
+//  
+// console.log(`Free memory is ${freeMemory}`)
+// console.log(`Uptime memory is ${upTime}`)
+// console.log(`Total memory is ${totalMemory}`)
+//file system 
+// const fs = require('fs');
+// const files = fs.readdirSync('./')
+// console.log((files));
+
+//event method
+
+// const EventEmitter = require('events');
+// const emitter = new EventEmitter();
+
+// //Register a listner
+// emitter.on('messageLogged',function() {
+//     console.log("Listener called");
+// });
+
+// //Raise a event
+// emitter.emit('messageLogged');
