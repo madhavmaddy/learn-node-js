4fcc3bfe34b30a56dcca1088506bdd260c195471
madhavmaddy <madhav@getchaine.com>
2021-01-12 23:49:29 +0530

    excluded node modules

--- app.js
+++ app.js
@@ -25,15 +25,70 @@
 // const files = fs.readdirSync('./')
 // console.log((files));
 
-//event method
+// event method
 
 // const EventEmitter = require('events');
 // const emitter = new EventEmitter();
 
-// //Register a listner
-// emitter.on('messageLogged',function() {
-//     console.log("Listener called");
+// // Register a listner
+// emitter.on('messageLogged',function(arg) {
+//     console.log("Listener called",arg);
 // });
 
 // //Raise a event
-// emitter.emit('messageLogged');
+// emitter.emit('messageLogged',{id: 1, name: 'Madhav'});
+
+//Creating an extending event emitter
+
+// const EventEmitter = require('events');
+
+
+// const Logger = require('./logger')
+// const logger = new Logger();
+
+// //Raising an event
+// logger.on('messageLogged', (arg) => {
+//     console.log('Listner called',arg);
+// })
+
+// logger.log('message')
+
+//http in node simple way
+
+// const http = require('http');
+
+// const server = http.createServer();
+
+// server.on('connection',(socket) => {
+//     console.log('New connection....')
+// })
+
+// server.listen(3000);
+
+// console.log('listening new port');
+
+//http in node added extra data or routes
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+
+    if(req.url === '/'){
+        res.write("Hello World");
+        res.end();
+    }
+    if(req.url === '/api/course'){
+        res.write(JSON.stringify([1,2,3]));
+        res.end();
+    }
+});
+
+server.on('connection',(socket) => {
+    console.log('New connection....')
+})
+
+server.listen(3000);
+
+console.log('listening new port');
+
+
--- logger.js
+++ logger.js
@@ -1,12 +1,26 @@
 
-console.log(__filename);
-console.log(__dirname);
-const url = "http://logger.io"
-//exporting this function to app.js to print the result
-function log(message) {
-    console.log(message)
-}
+// console.log(__filename);
+// console.log(__dirname);
+// const url = "http://logger.io"
+// //exporting this function to app.js to print the result
+// function log(message) {
+//     console.log(message)
+// }
 
-module.exports.result = log;
+// module.exports.result = log;
 
-module.exports.endPoint = url;
+// module.exports.endPoint = url;
+
+//Adding event emitter
+// const EventEmitter = require('events');
+
+// //creating a class
+// class Logger extends EventEmitter {
+
+//     log(message){
+//         console.log(message);
+//         this.emit('messageLogged', {id: 1, name: "Madhav"})
+//     }
+// }
+
+// module.exports = Logger;
