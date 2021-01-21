bc5655cf5aaba44dcf1bed24abd539386837decf
madhavmaddy <madhav@getchaine.com>
2021-01-21 13:14:13 +0530

    middle ware function

--- app.js
+++ app.js
@@ -69,30 +69,146 @@
 
 //http in node added extra data or routes
 
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer((req, res) => {
+// const server = http.createServer((req, res) => {
 
-    if(req.url === '/'){
-        res.write("Hello World");
-        res.end();
-    }
-    if(req.url === '/api/course'){
-        res.write(JSON.stringify([1,2,3]));
-        res.end();
-    }
-});
+//     if(req.url === '/'){
+//         res.write("Hello World");
+//         res.end();
+//     }
+//     if(req.url === '/api/course'){
+//         res.write(JSON.stringify([1,2,3]));
+//         res.end();
+//     }
+// });
 
-server.on('connection',(socket) => {
-    console.log('New connection....')
+// server.on('connection',(socket) => {
+//     console.log('New connection....')
+// })
+
+// server.listen(3000);
+
+// console.log('listening new port');
+
+// Express
+const express = require('express');
+const app = express();
+const Joi = require('joi');
+app.use(express.json());
+//custom middleware function the use of middle ware function is to call other functions using express. using next();
+app.use(function (req,res,next) {
+    console.log("logging.....")
+    next();
+})
+app.use(function(req,res,next){
+    console.log('Authentication')
+    next();
 })
 
-server.listen(3000);
+const courses = [ {id: 1, name: "course 1"},{id: 2, name: "course 2"},{id: 3, name: "course 3"} ]
 
-console.log('listening new port');
+// app.get('/', (req, res) => {
+//   res.send('GET request to the homepage')
+// });
 
-<<<<<<< HEAD
-//new functionality added on branch in andrewbug12 added
-=======
-//new functionality added on branch in andrewbug12 modified
->>>>>>> andrewbug
+// app.get('/course',(req,res) => {
+//     res.send([1,2,3])
+// })
+//:id is parameters
+// app.get('/api/courses/:id', (req, res) => {
+//   res.send(req.params.id);
+// });
+
+//getting params for year and month
+
+// app.get('/api/:year/:month', (req, res) => {
+//   res.send(req.params)
+// })
+//getting a specific course from courses array
+app.get('/api/v1/getcourse/:number', (req, res) => {
+ const course = courses.find(c => c.id === parseInt(req.params.number))
+ if(!course) res.status(404).send('This course with the given ID was not found');
+ res.send(course);
+})
+
+//getting all courses from courses array
+app.get('/api/listCourses', (req, res) => {
+  res.send(courses)
+})
+
+//post method
+// app.post('/api/courses', function (req, res) {
+//   res.send('POST request to the homepage')
+// })
+//adding items into courses using post method by javascript
+// app.post('/api/addcourse', function (req, res) {
+//     if(!req.body.name){
+//         res.status(400).send('Name field is not found.')
+//         return;
+//     }
+//   const course ={
+//       id: courses.length + 1,
+//       name: req.body.name
+//   };
+//   courses.push(course);
+//   res.send(course);
+// })
+
+//adding items into courses using Joi for validation and error messages
+app.post('/api/addcourse', function (req, res) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    }
+
+    const result = Joi.validate(req.body, schema);
+
+    if(result.error){
+        res.status(400).send(result.error.details[0].message)
+        return;
+    }
+  const course ={
+      id: courses.length + 1,
+      name: req.body.name
+  };
+  courses.push(course);
+  res.send(course);
+})
+
+//put method
+
+app.put('/api/courses/:id', (req,res) => {
+    //checking the course is exists or not
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+ if(!course) res.status(404).send('This course with the given ID was not found');
+    
+ //validate
+
+const { error } = validateCourse(req.body)
+if(error){
+    res.status(400).send(error.details[0].message)
+    return;
+};
+
+//update course
+course.name = req.body.name;
+res.send(course);
+
+//validatecourse
+function validateCourse(course) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    return Joi.validate(course, schema);
+}
+
+
+
+})
+//PORT
+//adding env port using export PORT = 5000 (without semicolon) using terminal 
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening to port number: ${port}`));
+
+
